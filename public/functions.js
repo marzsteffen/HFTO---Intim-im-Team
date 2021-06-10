3e182a5de6741e90bc2df5b882dcd3e3c1371db9
Steffen Marz <marz.steffen00@googlemail.com>
2021-06-10 15:52:12 +0200

    asdasd

--- public/functions.js
+++ public/functions.js
@@ -1,8 +1,24 @@
+function umfragePage(pageName) {
+
+    let umfragecontent = document.querySelectorAll(".umfragecontent");
+    for (let i = 0; i < umfragecontent.length; i++) {
+        umfragecontent[i].style.display = "none";
+    }
+    document.querySelector("#" + pageName).style.display = "block";
+    results();
+
+}
+
+function results() {
+
+}
+
 function quizPage(pageName) {
+    results();
     let quizcontent = document.querySelectorAll(".quizcontent");
     for (let i = 0; i < quizcontent.length; i++) {
         quizcontent[i].style.display = "none";
     }
-    let quizpage =  document.querySelector("#"+pageName);
-    quizpage.style.display = "block";
+    document.querySelector("#" + pageName).style.display = "block";
+
 }
