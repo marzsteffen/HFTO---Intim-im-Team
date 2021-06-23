c834865df3439fca6d344940b16abf76c3094adc
Sina Woelki <sina.woelki@gmail.com>
2021-06-23 10:59:39 +0200

    1

--- public/functions.js
+++ public/functions.js
@@ -15,24 +15,15 @@ function results() {
 
 let points = 0;
 
-function quizPage(pageName) {
 
+
+
+function quizPage2(pageName) {
     let quizcontent = document.querySelectorAll(".quizcontent");
     let checked1 = document.querySelector("#f1a1").checked;
     let checked2 = document.querySelector("#f1a2").checked;
     let checked3 = document.querySelector("#f1a3").checked;
-    let checked4 = document.querySelector("#f2a1").checked;
-    let checked5 = document.querySelector("#f2a2").checked;
-    let checked6 = document.querySelector("#f2a3").checked;
-    let checked7 = document.querySelector("#f3a1").checked;
-    let checked8 = document.querySelector("#f3a2").checked;
-    let checked9 = document.querySelector("#f3a3").checked;
-    let checked10 = document.querySelector("#f4a1").checked;
-    let checked11 = document.querySelector("#f4a2").checked;
-    let checked12 = document.querySelector("#f4a3").checked;
-    let checked13 = document.querySelector("#f5a1").checked;
-    let checked14 = document.querySelector("#f5a2").checked;
-    let checked15 = document.querySelector("#f5a3").checked;
+
 
 
 
@@ -41,10 +32,100 @@ function quizPage(pageName) {
             quizcontent[i].style.display = "none";
 
         }
-        document.querySelector("#" + pageName).style.display = "block";
-        if (checked1 || checked5 || checked8 || checked12 || checked14) {
+        if (checked1) {
             points++;
-            window.alert(points);
+        }
+        document.querySelector("#" + pageName).style.display = "block";
+
+    }
+}
+
+function quizPage3(pageName) {
+    let quizcontent = document.querySelectorAll(".quizcontent");
+    let checked4 = document.querySelector("#f2a1").checked;
+    let checked5 = document.querySelector("#f2a2").checked;
+    let checked6 = document.querySelector("#f2a3").checked;
+
+
+
+
+    if (checked4 || checked5 || checked6) {
+        for (let i = 0; i < quizcontent.length; i++) {
+            quizcontent[i].style.display = "none";
+
+        }
+        document.querySelector("#" + pageName).style.display = "block";
+        if (checked5) {
+            points++;
         }
     }
+
+}
+
+function quizPage4(pageName) {
+    let quizcontent = document.querySelectorAll(".quizcontent");
+
+    let checked7 = document.querySelector("#f3a1").checked;
+    let checked8 = document.querySelector("#f3a2").checked;
+    let checked9 = document.querySelector("#f3a3").checked;
+
+
+
+
+    if (checked7 || checked8 || checked9) {
+        for (let i = 0; i < quizcontent.length; i++) {
+            quizcontent[i].style.display = "none";
+
+        }
+        document.querySelector("#" + pageName).style.display = "block";
+        if (checked8) {
+            points++;
+        }
+    }
+
+}
+
+function quizPage5(pageName) {
+
+    let quizcontent = document.querySelectorAll(".quizcontent");
+    let checked10 = document.querySelector("#f4a1").checked;
+    let checked11 = document.querySelector("#f4a2").checked;
+    let checked12 = document.querySelector("#f4a3").checked;
+
+
+
+
+    if (checked10 || checked11 || checked12) {
+        for (let i = 0; i < quizcontent.length; i++) {
+            quizcontent[i].style.display = "none";
+
+        }
+        document.querySelector("#" + pageName).style.display = "block";
+        if (checked12) {
+            points++;
+        }
+    }
+}
+
+function quizPage6(pageName) {
+
+    let quizcontent = document.querySelectorAll(".quizcontent");
+    let checked13 = document.querySelector("#f5a1").checked;
+    let checked14 = document.querySelector("#f5a2").checked;
+    let checked15 = document.querySelector("#f5a3").checked;
+
+
+
+    if (checked13 || checked14 || checked15) {
+        for (let i = 0; i < quizcontent.length; i++) {
+            quizcontent[i].style.display = "none";
+
+        }
+        document.querySelector("#" + pageName).style.display = "block";
+        if (checked14) {
+            points++;
+        }
+    }
+
+    document.querySelector('#' + pageName).innerHTML = "<h1>Danke für ihre Teilnahme!</h1> <p> Sie haben " + points + " von 5 Punkten erreicht";
 }
